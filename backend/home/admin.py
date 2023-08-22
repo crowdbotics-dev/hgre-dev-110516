1a4bbe3bbdb4ff63e1ff6b2c71288e652c6dca1e
crowdbotics-dev <engineering@crowdbotics.com>
2023-08-22 17:04:42 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Vwcss
+admin.site.register(Vwcss)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Vwcss(models.Model):
+    'Generated Model'
+    vcbdbd = models.BigIntegerField()
+    ewtet = models.BigIntegerField()
